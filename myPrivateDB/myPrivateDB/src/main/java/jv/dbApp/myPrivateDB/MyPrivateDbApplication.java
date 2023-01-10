423dce0f74c21adb878cc6f010a5c500ef3926cd
EON-DC <mun0392@gmail.com>
2023-01-10 14:28:54 +0900

    init

---
+++ myPrivateDB/myPrivateDB/src/main/java/jv/dbApp/myPrivateDB/MyPrivateDbApplication.java
@@ -0,0 +1,13 @@
+package jv.dbApp.myPrivateDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrivateDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrivateDbApplication.class, args);
+	}
+
+}
---
+++ myPrivateDB/myPrivateDB/src/test/java/jv/dbApp/myPrivateDB/MyPrivateDbApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.dbApp.myPrivateDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPrivateDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
