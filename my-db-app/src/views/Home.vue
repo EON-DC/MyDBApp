679effd68a20be39400409628688e9788aad1aaa
EON-DC <mun0392@gmail.com>
2023-01-15 23:27:24 +0900

    save

---
+++ my-db-app/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>My saved words</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'study',
+  components: {},
+  props: ['nameOfChild', 'words'],
+  data() {
+    return {}
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ my-db-app/src/views/Study.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>My Quiz</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StudyView',
+  props: {
+    quiz: {
+      concept: String,
+      answerIndex: Number,
+      questionArray: Array
+    }
+  },
+  data() {
+    return {
+      pickedNum: null,
+      isAnswer: false,
+      isClicked: false
+    }
+  },
+  methods: {
+    setIsClicked() {
+      this.isClicked = true
+    }
+  }
+}
+</script>
